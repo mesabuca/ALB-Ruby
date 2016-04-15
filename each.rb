c8a3a06b2238dec22eb4f67ef9af9a7d615508c9
mesabuca <msbuyukcaglayan@gmail.com>
2016-04-15 12:36:35 +0300

    And Also

--- each.rb
+++ each.rb
@@ -1,9 +1,9 @@
 def multiplicate (odds)
-#odds = [1,3,5]
+    #odds = [1,3,5]
 
-odds.each do |num|
-  num *= 2
-  puts "#{num}"
+    odds.each do |num|
+    num *= 2
+    puts "#{num}"
 end
 
 end
@@ -13,11 +13,19 @@ array=Array.new
 puts "Enter three number"
 i=0
 until i==3
-array[i]=gets.chomp.to_i
-i+=1
+    array[i]=gets.chomp.to_i
+    i+=1
 end 
 
 #ali=[1,3,5]
 
 multiplicate(array)
 
+=begin
+    and also we can use 
+
+    array.each{|num| puts num*2}
+
+    like that
+
+=end
