f3f43b312760837351b3516e8758db12054432f9
mesabuca <msbuyukcaglayan@gmail.com>
2016-04-15 12:15:25 +0300

    How to use each

---
+++ each.rb
@@ -0,0 +1,23 @@
+def multiplicate (odds)
+#odds = [1,3,5]
+
+odds.each do |num|
+  num *= 2
+  puts "#{num}"
+end
+
+end
+
+array=Array.new
+
+puts "Enter three number"
+i=0
+until i==3
+array[i]=gets.chomp.to_i
+i+=1
+end 
+
+#ali=[1,3,5]
+
+multiplicate(array)
+
